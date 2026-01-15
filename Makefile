c87784582725b21a68c9d71349b9345962513b06
vivaz-ca <vivaz-ca@student.42lisboa.com>
2026-01-15 10:37:18 +0000

    added makefile + libx(lixo)

---
+++ Makefile
@@ -0,0 +1,71 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/01/15 10:25:55 by vivaz-ca          #+#    #+#              #
+#    Updated: 2026/01/15 10:33:32 by vivaz-ca         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+NAME = cub3d
+SRCDIR = .
+OBJDIR = objs
+MLXDIR = ../.minilibx-linux
+GNL_DIR = ./gnl
+MAP_DIR = maps
+TEXTURE_DIR = ../textures
+
+SRC = \
+	$(SRCDIR)/main.c gnl.c\
+
+OBJS = $(SRC:../%.c=$(OBJDIR)/%.o)
+OBJS := $(OBJS:./%.c=$(OBJDIR)/%.o)
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g
+
+MLX_FLAGS = -L . -lmlx -lXext -lX11
+INCLUDES = -I$(MLXDIR) -I$(GNL_DIR) -I. -I$(MAP_DIR)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) $(MLX_FLAGS) -o $(NAME)
+
+$(OBJDIR)/%.o: ../%.c
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+$(OBJDIR)/%.o: ./%.c
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+clean:
+	@rm -rf $(OBJDIR)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+# val:
+# 	valgrind --leak-check=full --track-fds=yes --show-leak-kinds=all --track-origins=yes ./$(NAME) $(MAP_DIR)/subject_map.ber
+	
+lib:
+	tar -xf minilibx-linux.tgz
+	cd minilibx-linux && make CFLAGS="-O3 -std=gnu11"
+	test -f libmlx.a
+	mv mlx.h ../
+	rm -rf minilibx-linux
+
+norm:
+	@norminette $(shell find . -type f \( -name "*.c" -o -name "*.h" \)) \
+	| awk '/c: Error/ { c++; if (c % 2 == 1) printf "\033[1;35m%s\033[0m\n", $$0; else printf "\033[1;36m%s\033[0m\n", $$0 }'
+	@echo "Amount of errors: " && norminette $(shell find . -type f \( -name "*.c" -o -name "*.h" \)) | grep "Error" | wc -l
+
+
+.PHONY: all clean fclean re
---
+++ gnl.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vivaz-ca <vivaz-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 10:28:17 by vivaz-ca          #+#    #+#             */
+/*   Updated: 2026/01/15 10:28:18 by vivaz-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+# define BUFFER_SIZE 2
+
+size_t ft_strlen(char *str)
+{
+    size_t i = 0;
+    if (!str)
+        return (0);
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strchr(char *str, int c)
+{
+    int i = 0;
+    if (!str)
+        return (NULL);
+    if (c == '\0')
+        return ((char *)str + ft_strlen(str));
+    while (str[i])
+    {
+        if (str[i] == c)
+            return ((char *)str + i);
+        i++;
+    }
+    return (NULL);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+    char *joined = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    if (!joined)
+        return (NULL);
+    int i = 0, j = 0;
+    if (s1)
+    {
+        while (s1[i] != '\0')
+        {
+            joined[i] = s1[i];
+            i++;
+        }
+    }
+    while (s2[j] != '\0')
+    {
+        joined[i + j] = s2[j];
+        j++;
+    }
+    joined[i + j] = '\0';
+    free(s1);
+    return (joined);
+}
+
+char *full_line(char *full_line, char *file_str)
+{
+    int i = 0, j = 0;
+    while (file_str[i] != '\0' && file_str[i] != '\n')
+        i++;
+    if (file_str[i] == '\n')
+        i++;
+    char *temp = malloc(i + 1);
+    if (!temp)
+        return (NULL);
+    while (j < i)
+    {
+        temp[j] = file_str[j];
+        j++;
+    }
+    temp[j] = '\0';
+    j = 0;
+    while (file_str[i] != '\0')
+        file_str[j++] = file_str[i++];
+    file_str[j] = '\0';
+    full_line = ft_strjoin(full_line, temp);
+    free(temp);
+    return (full_line);
+}
+
+char *get_next_line(int fd)
+{
+    static char file_str[BUFFER_SIZE + 1];
+    char *print = NULL;
+    int count;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    while (1)
+    {
+        if (file_str[0] == '\0')
+        {
+            count = read(fd, file_str, BUFFER_SIZE);
+            if (count <= 0)
+                break ;
+            file_str[count] = '\0';
+        }
+        if (ft_strchr(file_str, '\n'))
+            return (full_line(print, file_str));
+        print = ft_strjoin(print, file_str);
+        file_str[0] = '\0';
+    }
+    if (count < 0)
+        return (free(print), NULL);
+    return (print);
+}
